1a4df2e0f54b57ccda6325ff1b09f66bb67bed60
j-signorelli <jms26@illinois.edu>
2023-01-05 08:49:12 -0600

    Added UpdateBC call

--- adapter_files/precice.cpp
+++ adapter_files/precice.cpp
@@ -703,7 +703,9 @@ double Precice::advance(double computedTimestepLength) {
           geometry_container[ZONE_0][INST_0][MESH_0]->vertex[valueMarkerWet[i]][iVertex]->SetVarCoord(
               displacementDeltas_su2[iVertex]);
         } else {  // Else we are doing CHT
-          geometry_container[ZONE_0][INST_0][MESH_0]->SetCustomBoundaryTemperature(valueMarkerWet[i], iVertex, 500);//temperatures[iVertex]);
+          geometry_container[ZONE_0][INST_0][MESH_0]->SetCustomBoundaryTemperature(valueMarkerWet[i], iVertex, temperatures[iVertex]);
+          geometry_container[ZONE_0][INST_0][MESH_0]->UpdateCustomBoundaryConditions(geometry_container[ZONE_0][INST_0][MESH_0], config_container[ZONE_0]);
+          
         }
       }
 
